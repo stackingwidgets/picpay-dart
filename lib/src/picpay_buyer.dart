1c9a0ff9051648a2a404f271a059bd7b2e022cc5
Luiz Eduardo <luizeof@gmail.com>
2020-02-05 09:58:55 -0300

    Update picpay_buyer.dart

--- lib/src/picpay_buyer.dart
+++ lib/src/picpay_buyer.dart
@@ -1,10 +1,21 @@
+/// Objeto contendo as informações do comprador.
 class PicPayBuyer {
+  /// Primeiro nome do comprador.
   final String firstName;
+
+  /// Sobrenome do comprador.
   final String lastName;
+
+  /// CPF do comprador no formato 123.456.789-10
   final String document;
+
+  /// E-mail do comprador.
   final String email;
+
+  /// Numero de telefone do comprador no formato +55 27 12345-6789
   final String phone;
 
+  /// Objeto contendo as informações do comprador.
   PicPayBuyer(
     this.firstName,
     this.lastName,
